7defad143b43e68114433373aa890dafa3c0dd9b
CavanOrBxj <40481651+CavanOrBxj@users.noreply.github.com>
2018-08-09 13:39:51 +0800

    代码上传

---
+++ wavdeal/mainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace wavdeal
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWavForlder = new System.Windows.Forms.TextBox();
+            this.txtMp3Forlder = new System.Windows.Forms.TextBox();
+            this.btnSelectWav = new System.Windows.Forms.Button();
+            this.btnSelectMp3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(336, 234);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(107, 43);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "启动转换";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectMp3);
+            this.groupBox1.Controls.Add(this.btnSelectWav);
+            this.groupBox1.Controls.Add(this.txtMp3Forlder);
+            this.groupBox1.Controls.Add(this.txtWavForlder);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 202);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "音频文件夹路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MP3存放路径：";
+            // 
+            // txtWavForlder
+            // 
+            this.txtWavForlder.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtWavForlder.Location = new System.Drawing.Point(106, 62);
+            this.txtWavForlder.Name = "txtWavForlder";
+            this.txtWavForlder.ReadOnly = true;
+            this.txtWavForlder.Size = new System.Drawing.Size(287, 21);
+            this.txtWavForlder.TabIndex = 2;
+            // 
+            // txtMp3Forlder
+            // 
+            this.txtMp3Forlder.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMp3Forlder.Location = new System.Drawing.Point(106, 125);
+            this.txtMp3Forlder.Name = "txtMp3Forlder";
+            this.txtMp3Forlder.ReadOnly = true;
+            this.txtMp3Forlder.Size = new System.Drawing.Size(287, 21);
+            this.txtMp3Forlder.TabIndex = 3;
+            // 
+            // btnSelectWav
+            // 
+            this.btnSelectWav.Location = new System.Drawing.Point(399, 61);
+            this.btnSelectWav.Name = "btnSelectWav";
+            this.btnSelectWav.Size = new System.Drawing.Size(41, 23);
+            this.btnSelectWav.TabIndex = 4;
+            this.btnSelectWav.Text = "选择";
+            this.btnSelectWav.UseVisualStyleBackColor = true;
+            this.btnSelectWav.Click += new System.EventHandler(this.btnSelectWav_Click);
+            // 
+            // btnSelectMp3
+            // 
+            this.btnSelectMp3.Location = new System.Drawing.Point(400, 124);
+            this.btnSelectMp3.Name = "btnSelectMp3";
+            this.btnSelectMp3.Size = new System.Drawing.Size(40, 23);
+            this.btnSelectMp3.TabIndex = 5;
+            this.btnSelectMp3.Text = "选择";
+            this.btnSelectMp3.UseVisualStyleBackColor = true;
+            this.btnSelectMp3.Click += new System.EventHandler(this.btnSelectMp3_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 293);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainForm";
+            this.Text = "音频转换服务端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMp3Forlder;
+        private System.Windows.Forms.TextBox txtWavForlder;
+        private System.Windows.Forms.Button btnSelectMp3;
+        private System.Windows.Forms.Button btnSelectWav;
+    }
+}
+
---
+++ wavdeal/mainForm.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace wavdeal
+{
+    public partial class mainForm : Form
+    {
+
+        string sWavFolder = "";//定义存放Wav的文件夹路径
+        string sMp3Folder = "";//定义存放转换成mp3文件的文件夹路径
+        private IniFiles ini;
+        FileSystemWatcher fswWavFileForder = null;//文件监控器。
+        public Object olLock;  //文件列表list操作锁
+        Thread thDealCompressor = null;
+        List<string> lwavfiles = null;//初始化文件列表
+
+        //private delegate void setLogTextDelegate(FileSystemEventArgs e);
+
+        //private delegate void renamedDelegate(RenamedEventArgs e);
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region 文件转换
+        private static string mp3CoderWavToMp3(string wavFile, string outmp3File)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = @Application.StartupPath + "\\mp3Coder\\mp3Coder.exe";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            psi.CreateNoWindow = true;
+            if (wavFile.IndexOf(' ') > 0)
+            {
+                wavFile = "\"" + wavFile + "\"";
+            }
+            if (outmp3File.IndexOf(' ') > 0)
+            {
+                outmp3File = "\"" + outmp3File + "\"";
+            }
+            psi.Arguments = wavFile + " " + outmp3File;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = Process.Start(psi);
+            p.StartInfo.RedirectStandardOutput = true;
+            string strRst = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            //获取结果
+            //Log.Instance.LogWrite("48行-转换文件:" + wavFile);
+            if (strRst.Contains("Done: 100.00%"))
+            {
+                //MessageBox.Show("完成转换！");
+                return "OK";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+
+        private static string LameWavToMp3(string wavFile, string outmp3File)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = @Application.StartupPath + "\\lame.exe";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            psi.CreateNoWindow = true;
+            if (wavFile.IndexOf(' ') > 0)
+            {
+                wavFile = "\"" + wavFile + "\"";
+            }
+            if (outmp3File.IndexOf(' ') > 0)
+            {
+                outmp3File = "\"" + outmp3File + "\"";
+            }
+            psi.Arguments = "-V2 " + wavFile + " " + outmp3File;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = Process.Start(psi);
+            p.StartInfo.RedirectStandardOutput = true;
+            string strRst = p.StandardOutput.ReadToEnd();
+            p.WaitForExit(500);
+            return strRst;
+        }
+
+        private static string shellwavtomp3(string wavFile, string outmp3File)
+        {
+            try
+            {
+                //实例一个process类
+                Process process = new Process();//设定程序名
+                process.StartInfo.FileName = "cmd.exe";//使用cmd
+                process.StartInfo.UseShellExecute = false;//关闭Shell的使用
+                //重新定向标准输入，输入，错误输出
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;//设置cmd窗口不显示
+                process.Start();//开始
+                //输入命令，退出
+                if (wavFile.IndexOf(' ') > 0)
+                {
+                    wavFile = "\"" + wavFile + "\"";
+                }
+                if (outmp3File.IndexOf(' ') > 0)
+                {
+                    outmp3File = "\"" + outmp3File + "\"";
+                }
+                string sShell = "mp3Coder " + wavFile + " " + outmp3File;
+                process.StandardInput.WriteLine(sShell);
+                process.StandardInput.WriteLine("exit");
+                Thread.Sleep(500);
+                string strRst = process.StandardOutput.ReadToEnd();
+                //获取结果
+                if (strRst.Contains("Done: 100.00%"))
+                {
+                    MessageBox.Show("完成转换！");
+                    return "OK";
+                }
+                else
+                {
+                    return "NO";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "NO";
+            }
+            //process.WaitForExit();
+        }
+        #endregion End
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sWavfilepath = @Application.StartupPath + "\\rec_2015_8_24_16_12_1_727.wav";
+            string sMp3FilePath = @Application.StartupPath + "\\mp3files\\" + System.DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".mp3";
+            string msg = mp3CoderWavToMp3(sWavfilepath, sMp3FilePath);
+            MessageBox.Show(msg);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            if (btnConvert.Text == "启动转换")
+            {
+                #region 检查文件夹设置
+                if (sWavFolder == "")
+                {
+                    MessageBox.Show("请设置Wav文件存放的文件夹！");
+                    return;
+                }
+                else
+                {
+                    if (!Directory.Exists(sWavFolder))
+                    {
+                        MessageBox.Show("设置的Wav文件夹路径不存在！");
+                        return;
+                    }
+                }
+                if (sMp3Folder == "")
+                {
+                    MessageBox.Show("请设置Mp3文件存放的文件夹！");
+                }
+                else
+                {
+                    if (!Directory.Exists(sMp3Folder))
+                    {
+                        MessageBox.Show("设置的mp3文件夹路径不存在！");
+                        return;
+                    }
+                }
+                #endregion End
+
+                //先把wav文件夹下已经有的文件载入到
+                DirectoryInfo theFolder = new DirectoryInfo(sWavFolder);
+                FileInfo[] wavfiles = theFolder.GetFiles("*.wav");
+                if (wavfiles.Length > 0)
+                {
+                    lwavfiles.Clear();//清除掉残余转换文件列表，重新加入转换列表。
+                    for (int i = 0; i < wavfiles.Length; i++)
+                    {
+                        //int wfs = wavfiles[i].Name.Length;
+                        //string wavfilesuffix = wavfiles[i].Name.Substring(wfs -3,3).ToLower();
+                        //if (wavfilesuffix == "wav")
+                        //{
+                        //    lwavfiles.Add(sWavFolder + "\\" + wavfiles[i].Name);
+                        //}
+                        lwavfiles.Add(sWavFolder + "\\" + wavfiles[i].Name);
+                    }
+                }
+                thDealCompressor = new Thread(DealCompressor);
+                thDealCompressor.Name = "";
+                thDealCompressor.IsBackground = true;
+                thDealCompressor.Start();
+                fswWavFileForder.EnableRaisingEvents = true;//启用该组件
+                btnConvert.Text = "停止转换";
+            }
+            else
+            {
+                fswWavFileForder.EnableRaisingEvents = false;//启用该组件
+                btnConvert.Text = "启动转换";
+                if (thDealCompressor != null)
+                {
+                    thDealCompressor.Abort();
+                    thDealCompressor = null;
+                }
+            }
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            //初始化文件夹路径
+            lwavfiles = new List<string>();//初始化
+            olLock = new Object();
+
+            ini = new IniFiles(@Application.StartupPath + "\\Config.ini");//
+
+            sWavFolder = ini.ReadValue("FilesPath", "WavFilesPath"); // @Application.StartupPath + "\\wavfiles";
+            sMp3Folder = ini.ReadValue("FilesPath", "Mp3FilesPath"); //@Application.StartupPath + "\\mp3files";
+            txtMp3Forlder.Text = sMp3Folder;
+            txtWavForlder.Text = sWavFolder;
+            if (!Directory.Exists(sMp3Folder))
+            {
+                MessageBox.Show("设置的Mp3文件夹路径不存在,请在配置文件下配置好在运行！");
+                this.Close();
+                return;
+            }
+            if (!Directory.Exists(sWavFolder))
+            {
+                MessageBox.Show("设置的Wav文件夹路径不存在,请在配置文件下配置好在运行！");
+                this.Close();
+                return;
+            }
+
+            fswWavFileForder = new FileSystemWatcher();
+            fswWavFileForder.Filter = "*.wav";
+            fswWavFileForder.IncludeSubdirectories = false;//不监视子路径
+            fswWavFileForder.Path = sWavFolder;
+            fswWavFileForder.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Size | NotifyFilters.CreationTime;//
+            fswWavFileForder.Created += new FileSystemEventHandler(this.fswWavFileForder_EventHandle);
+            //fswWavFileForder.EnableRaisingEvents = true;//启用该组件
+
+            //
+            string sLogPath = Application.StartupPath + "\\Log";
+            if (!System.IO.Directory.Exists(sLogPath))
+                System.IO.Directory.CreateDirectory(sLogPath);
+            Log.Instance.LogDirectory = sLogPath + "\\";
+            Log.Instance.FileNamePrefix = "WavToMp3";
+            Log.Instance.CurrentMsgType = MsgLevel.Debug;
+            Log.Instance.logFileSplit = LogFileSplit.Daily;
+            Log.Instance.MaxFileSize = 2;
+            Log.Instance.InitParam();
+
+
+
+
+            btnConvert_Click(null, null);
+        }
+
+        private void fswWavFileForder_EventHandle(object sender, FileSystemEventArgs e)
+        {
+            string[] strArr = e.FullPath.Split(new char[] { '\\' });
+            if (!strArr[1].Equals("$RECYCLE.BIN"))  //回收站
+            {
+                //(e.ChangeType.ToString(); WatcherChangeTypes.Created等
+                //strArr[strArr.Length - 1]); //文件名
+                //e.FullPath;
+                //Log.Instance.LogWrite("243行:" + e.FullPath);
+                lock (olLock)
+                {
+                    switch (e.ChangeType)
+                    {
+                        case WatcherChangeTypes.Created:
+                            lwavfiles.Add(e.FullPath);//
+                            //lwavfiles.Add(strArr[strArr.Length - 1]);//文件名列表
+                            break;
+                        case WatcherChangeTypes.Deleted:
+                            //lwavfiles.Remove(e.FullPath);//删除了的wav文件就移除
+                            break;
+                        case WatcherChangeTypes.Changed:
+
+                            break;
+                    }
+                }
+
+            }
+        }
+        //处理解压缩转换过程
+        private void DealCompressor()
+        {
+            List<string> lftmp = new List<string>();//临时文件列表
+            //处理增加文件列表
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        lock (olLock)
+                        {
+                            DirectoryInfo theLineFolder = new DirectoryInfo(sWavFolder);
+                            FileInfo[] linewavfiles = theLineFolder.GetFiles("*.wav");
+                            if (linewavfiles.Length > 0)
+                            {
+                                //lwavfiles.Clear();//清除掉残余转换文件列表，重新加入转换列表。
+                                for (int i = 0; i < linewavfiles.Length; i++)
+                                {
+                                    if (!lwavfiles.Contains(sWavFolder + "\\" + linewavfiles[i].Name))
+                                    {
+                                        lwavfiles.Add(sWavFolder + "\\" + linewavfiles[i].Name);
+                                    }
+                                }
+                            }
+                            if (lwavfiles.Count == 0)
+                            {
+                                Thread.Sleep(500);
+                                continue;
+                            }
+                            lftmp.InsertRange(0, lwavfiles);
+                            lwavfiles.Clear();
+                        }
+                    }
+                    catch (Exception exs)
+                    {
+                        Console.WriteLine(exs.Message);
+                        Log.Instance.LogWrite("330行-更新转换文件列表:" + exs.Message);
+                    }
+                    //处理文件列表
+                    for (int i = 0; i < lftmp.Count; i++)
+                    {
+                        //处理转换
+                        string[] strArr = lftmp[i].Split(new char[] { '\\' });
+                        string fname = strArr[strArr.Length - 1];//获取文件名
+                        fname = fname.Substring(0, fname.Length - 4);
+                        string sMp3FilePath = sMp3Folder + "\\" + fname + ".mp3";//以时间生成System.DateTime.Now.ToString("yyyyMMddHHmmssfff")
+                       
+                        if (File.Exists(sMp3FilePath))
+                        {
+                            File.Delete(lftmp[i]);//已经存在转换文件就删除原文件
+                            lftmp.Remove(lftmp[i]);
+                            i = i - 1;
+                            continue;
+                        }
+
+                        if (mp3CoderWavToMp3(lftmp[i], sMp3FilePath) == "OK")
+                        {
+                            //保存记录数据到数据库
+                            //Console.WriteLine("文件：" + lftmp[i] + " 已经转换！");
+                            //删除原wav文件
+                            try
+                            {
+                                if (File.Exists(lftmp[i]))
+                                {
+                                    File.Delete(lftmp[i]);
+                                    lftmp.Remove(lftmp[i]);
+                                }
+                                //File.Delete(@"" + lftmp[i]);
+                                i--;
+                            }
+                            catch(Exception ex)
+                            {
+                                //Log.Instance.LogWrite(ex.Message);
+                                Console.WriteLine(ex.Message);
+                                i--;
+                            }
+                            //进行下一步操作
+                            //i = i - 1;
+                        }
+                        else
+                        {
+                            Log.Instance.LogWrite("375行:" + sMp3FilePath);
+                            i--;
+                            continue;
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DealArry:" + ex.Message);
+                Log.Instance.LogWrite("385行－总的错误:" + ex.Message);
+            }
+        }
+
+        private void btnSelectWav_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                sWavFolder = dialog.SelectedPath;
+                txtWavForlder.Text = dialog.SelectedPath;
+                //MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fswWavFileForder.Path = sWavFolder;//设置监控文件夹
+            }
+        }
+
+        private void btnSelectMp3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                sMp3Folder = dialog.SelectedPath;
+                txtMp3Forlder.Text = dialog.SelectedPath;
+            }
+
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (thDealCompressor != null)
+            {
+                thDealCompressor.Abort();
+                thDealCompressor = null;
+            }
+
+        }
+    }
+}
